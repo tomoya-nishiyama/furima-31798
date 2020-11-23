5e7657668158c45671b838336bc7bb03f770de22
tomoya-nishiyama <a.lucky0630@gmail.com>
2020-11-23 12:42:40 +0900

    修正しました

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefectre_id   { 2 }
     city           { '東京' }
     house_number   { '1-1' }
+    building_name  { '1-1' }
     phone_number   { '176' }
     token          {"tok_abcdefghijk00000000000000000"}
   end
